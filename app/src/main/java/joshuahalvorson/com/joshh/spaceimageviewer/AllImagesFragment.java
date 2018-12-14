ce0424891592058fd1ffe906f6fe6c3a41d75f09
Josh Halvorson <Jhalvorson5@mail.greenriver.edu>
2018-12-14 14:09:16 -0800

    Delete unused code/comments.

--- app/src/main/java/joshuahalvorson/com/joshh/spaceimageviewer/AllImagesFragment.java
+++ app/src/main/java/joshuahalvorson/com/joshh/spaceimageviewer/AllImagesFragment.java
@@ -18,6 +18,7 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+
 public class AllImagesFragment extends Fragment {
 
     private RecyclerView recyclerView;
@@ -40,9 +41,6 @@ public class AllImagesFragment extends Fragment {
                     ft.add(R.id.image_details_container, imageDetailFragment, "image_details_key");
                     ft.addToBackStack(null);
                     ft.commit();
-                    //Log.i("sdadsaD", Integer.toString(getChildFragmentManager().getBackStackEntryCount()));
-
-                    //imageDetailFragment.setViewData(image);
                 }
             });
             recyclerView.setAdapter(adapter);
@@ -53,34 +51,23 @@ public class AllImagesFragment extends Fragment {
 
     }
 
-    // This event fires 1st, before creation of fragment or any views
-    // The onAttach method is called when the Fragment instance is associated with an Activity.
-    // This does not mean the Activity is fully initialized.
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
     }
 
-    // This event fires 2nd, before views are created for the fragment
-    // The onCreate method is called when the Fragment instance is being created, or re-created.
-    // Use onCreate for any standard setup that does not require the activity to be fully created
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         viewModel = ViewModelProviders.of(this).get(HubbleImageDetailsViewModel.class);
     }
 
-    // The onCreateView method is called when Fragment should create its View object hierarchy,
-    // either dynamically or via XML layout inflation.
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.all_images_fragment_layout, container, false);
     }
 
-    // This event is triggered soon after onCreateView().
-    // onViewCreated() is only called if the view returned from onCreateView() is non-null.
-    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -90,16 +77,11 @@ public class AllImagesFragment extends Fragment {
         recyclerView.setHasFixedSize(true);
     }
 
-    // This method is called when the fragment is no longer connected to the Activity
-    // Any references saved in onAttach should be nulled out here to prevent memory leaks.
     @Override
     public void onDetach() {
         super.onDetach();
     }
 
-    // This method is called after the parent Activity's onCreate() method has completed.
-    // Accessing the view hierarchy of the parent activity must be done in the onActivityCreated.
-    // At this point, it is safe to search for activity View objects by their ID, for example.
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -112,7 +94,4 @@ public class AllImagesFragment extends Fragment {
         viewModel.getImageList().removeObserver(observer);
     }
 
-    public void populateRecyclerView(){
-
-    }
 }
